c99f6ff1fff5107e61e3159f0da62e93a1b4c0c6
Jacob <47821203+iacobo@users.noreply.github.com>
2021-06-21 11:53:48 +0100

    Update and rename pytorch_bp.py to training.py

--- training.py
+++ training.py
@@ -14,12 +14,13 @@ from torch import nn
 from torch.optim import SGD
 from torch.nn import CrossEntropyLoss
 
-from avalanche.models import SimpleMLP
 from avalanche.training.strategies import Naive, JointTraining, Cumulative #Baselines
 from avalanche.training.strategies import EWC, LwF, SynapticIntelligence   #Regularisation
 from avalanche.training.strategies import Replay, GEM, AGEM                #Rehearsal
 #from avalanche.training.strategies import AR1, CWRStar, GDumb, CoPE, StreamingLDA #???
 from avalanche.benchmarks.generators import tensors_benchmark, ni_benchmark
+
+from models import SimpleMLP, SimpleCNN, SimpleLSTM, SimpleRNN
 
 def plot_accuracy(method, model, results, ax=None):
     ax = ax or plt.gca()
@@ -60,7 +61,9 @@ def clean_plot(i, j, ax=None):
 ######################
 # Loading data into 'stream' of 'experiences' (tasks)
 
-pattern_shape = (15, 8) # (time_steps, variables)
+n_timesteps = 15
+n_channels = 8
+pattern_shape = (n_timesteps, n_channels)
 n = 5 # Number of tasks
 
 # Definition of training experiences
@@ -94,93 +97,13 @@ print('Simul Data loaded!')
 # DEFINE MODEL
 #####################
 
-class SimpleRNN(nn.Module):
-    def __init__(self, input_size, n_channels, output_size=2, hidden_dim=10, n_layers=2):
-        super(SimpleRNN, self).__init__()
-
-        # Defining some parameters
-        self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-
-        #Defining the layers
-        # RNN Layer
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
-        # Fully connected layer
-        self.fc = nn.Linear(n_channels*hidden_dim, output_size)
-    
-    def forward(self, x):
-        
-        batch_size = x.shape[0]
-        
-        # Initializing hidden state for first input using method defined below
-        hidden = self.init_hidden(batch_size)
-
-        # Passing in the input and hidden state into the model and obtaining outputs
-        out, hidden = self.rnn(x, hidden)
-        
-        # Reshaping the outputs such that it can be fit into the fully connected layer
-        out = out.contiguous().view(batch_size, -1)
-        out = self.fc(out)
-        
-        return out
-    
-    def init_hidden(self, batch_size):
-        # This method generates the first hidden state of zeros which we'll use in the forward pass
-        # We'll send the tensor holding the hidden state to the device we specified earlier as well
-        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
-        return hidden
-
-class SimpleLSTM(nn.Module):
-
-    def __init__(self, input_size, n_channels, hidden_dim=10, output_size=2):
-        super(SimpleLSTM, self).__init__()
-
-        self.lstm = nn.LSTM(input_size, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(n_channels*hidden_dim, output_size)
-
-    def forward(self, x):
-        batch_size = x.size(0)
-
-        x, _ = self.lstm(x)
-        x = x.reshape(batch_size, -1)
-        x = self.fc(x)
-        return x
-
-class SimpleCNN(nn.Module):   
-    def __init__(self, n_channels):
-        super(SimpleCNN, self).__init__()
-
-        self.cnn_layers = nn.Sequential(
-            # Defining a 2D convolution layer
-            nn.Conv1d(n_channels, 4, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm1d(4),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-            # Defining another 2D convolution layer
-            nn.Conv1d(4, 4, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm1d(4),
-            nn.ReLU(inplace=True),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-        )
-
-        self.linear_layers = nn.Sequential(
-            nn.Linear(8, 2)
-        )
-
-    # Defining the forward pass    
-    def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
-
-mlp = SimpleMLP(input_size=np.product(pattern_shape), num_classes=2)
-cnn = SimpleCNN(n_channels=pattern_shape[-2])
-lstm = SimpleLSTM(input_size=pattern_shape[-1], n_channels=pattern_shape[-2])
-rnn = SimpleRNN(input_size=pattern_shape[-1], n_channels=pattern_shape[-2])
+cnn = SimpleCNN(n_channels=n_channels)
+mlp = SimpleMLP(input_size=n_timesteps, n_channels=n_channels)
+rnn = SimpleRNN(input_size=n_timesteps, n_channels=n_channels)
+lstm = SimpleLSTM(input_size=n_timesteps, n_channels=n_channels)
 models = {'MLP':mlp, 'CNN':cnn, 'RNN':rnn, 'LSTM:':lstm}
 
-print('Model defined!')
+print('Models defined!')
 
 ######################
 # Define CL strategy
