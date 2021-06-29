d3644ac592fa32dd0c9712c3f98fefdd8e30150b
Jacob <47821203+iacobo@users.noreply.github.com>
2021-06-29 16:20:31 +0100

    Update training.py

--- training.py
+++ training.py
@@ -1,77 +1,98 @@
-#%%
-####################
-# PLOTTING
-####################
-
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from collections import defaultdict
-
-import torch
-from torch import nn
+# ML imports
+from matplotlib import pyplot as plt
 from torch.optim import SGD
 from torch.nn import CrossEntropyLoss
 
+# CL imports
 from avalanche.training.strategies import Naive, JointTraining, Cumulative #Baselines
 from avalanche.training.strategies import EWC, LwF, SynapticIntelligence   #Regularisation
 from avalanche.training.strategies import Replay, GEM, AGEM                #Rehearsal
 #from avalanche.training.strategies import AR1, CWRStar, GDumb, CoPE, StreamingLDA #???
 from avalanche.benchmarks.generators import tensors_benchmark, ni_benchmark
 
+from avalanche.evaluation.metrics import accuracy_metrics
+from avalanche.training.plugins import EvaluationPlugin
+from avalanche.logging import InteractiveLogger
+
+# Local imports
 from models import SimpleMLP, SimpleCNN, SimpleLSTM, SimpleRNN
+from plotting import plot_accuracy, clean_plot, clean_subplot
+from data_processing import generate_experiences, generate_eeg_experiences, generate_permuted_eeg_experiences
 
-def plot_accuracy(method, model, results, ax=None):
-    ax = ax or plt.gca()
+#############
+# HELPER FUNCTIONS
+#############
 
-    acc = defaultdict(list)
+def load_strategy(model, strategy, train_epochs=10, eval_every=1, train_mb_size=128, eval_mb_size=1024, **kwargs):
+    """
+    """
+    optimizer = SGD(model.parameters(), lr=0.001, momentum=0.9)
+    criterion = CrossEntropyLoss()
 
-    for result in results:
-        for k,v in result.items():
-            if 'Top1_Acc_Exp' in k and '/Exp' in k:
-                new_k = k.split('/')[-1].replace('Exp00','').replace('Exp0','')
-                acc[new_k].append(v)
+    strategies = {'Naive':Naive, 'Joint':JointTraining, 'Cumulative':Cumulative,
+    'EWC':EWC, 'LwF':LwF, 'SI':SynapticIntelligence, 'Replay':Replay, 
+    'GEM':GEM, 'AGEM':AGEM}
 
-    df = pd.DataFrame.from_dict(acc, orient='index')
-    df.index.rename(f'Task introduced \n ({train_epochs} epochs per task)', inplace=True)
-    stacked = df.stack().reset_index()
-    stacked.rename(columns={'level_1': 'Task', 0: 'Accuracy'}, inplace=True)
+    strategy = strategies[strategy]
+    interactive_logger = InteractiveLogger()
 
-    # Only plot task accuracies after examples have been encountered
-    #stacked = stacked[stacked['Task'].astype(int) <= stacked['Task introduced \n (15 epochs per task)'].astype(int)]
+    eval_plugin = EvaluationPlugin(
+        accuracy_metrics(experience=True),# epoch=True, stream=True),
+        loggers=[interactive_logger])
 
-    sns.lineplot(data=stacked, x=f'Task introduced \n ({train_epochs} epochs per task)', y='Accuracy', hue='Task', ax=ax)
-    ax.title.set_text(method)
-    ax.set_ylabel(model)
+    model = strategy(
+        model, optimizer, criterion,
+        train_mb_size=train_mb_size, eval_mb_size=eval_mb_size,
+        train_epochs=train_epochs, eval_every=eval_every, evaluator=eval_plugin,
+        **kwargs
+    )
 
-def clean_plot(i, j, ax=None):
-    ax = ax or plt.gca()
-    if i!=0:
-        #ax.xaxis.label.set_visible(False)
-        ax.set_title('')
-    if j!=0:
-        ax.yaxis.label.set_visible(False)
-    if i!=0 or j!=0:
-        ax.get_legend().remove()
+    return model
 
+def train_method(cl_strategy, scenario):
+    """
+    """
+    print('Starting experiment...')
+
+    for experience in scenario.train_stream:
+        print(f'Start of experience: {experience.current_experience}')
+
+        cl_strategy.train(experience, eval_streams=[scenario.test_stream])
+        print('Training completed')
+
+    results = cl_strategy.evaluator.get_all_metrics()
+
+    return results
 
 ######################
 # DATA LOADING
 ######################
 # Loading data into 'stream' of 'experiences' (tasks)
 
-n_timesteps = 5
-n_channels = 2
-n_tasks = 5
+GENERATE_DATA = True
+USE_EEG_DATA = not GENERATE_DATA
+
+n_tasks = 4
 
 # Definition of training experiences
 # of form (x:(patients, variables, time_steps), y:(outcome))
 
-from data_processing import generate_experiences
+if GENERATE_DATA:
+    n_timesteps = 6
+    n_channels = 2
+    experiences = generate_experiences(n_timesteps, n_channels, n_tasks)
+    test_experiences = generate_experiences(n_timesteps, n_channels, n_tasks, test=True)
 
-experiences = generate_experiences(n_timesteps, n_channels, n_tasks)
-test_experiences = generate_experiences(n_timesteps, n_channels, n_tasks, test=True)
+elif USE_EEG_DATA:
+    # If you use too many task-splits, there will be all 0's or all 1's in test datasets
+    experiences, test_experiences = generate_eeg_experiences(n_tasks)
+    #experiences, test_experiences = generate_permuted_eeg_experiences(n_tasks)
+
+else:
+    input('No data specified... (Press Ctrl+C to quit')
+
+n_timesteps = experiences[0][0].shape[-2]
+n_channels = experiences[0][0].shape[-1]
 
 scenario = tensors_benchmark(
     train_tensors=experiences,
@@ -89,97 +110,63 @@ scenario = tensors_benchmark(
 #    task_labels=[0, 0],  # Task label of each train exp
 #)
 
-print('Simul Data loaded!')
+print('Data loaded!')
 
-#####################
-# DEFINE MODEL
-#####################
 
-cnn = SimpleCNN(n_channels=n_channels)
+######################
+# Define models
+######################
+
+cnn = SimpleCNN(n_channels=n_channels, seq_len=n_timesteps)
 mlp = SimpleMLP(n_channels=n_channels, seq_len=n_timesteps)
 rnn = SimpleRNN(n_channels=n_channels, seq_len=n_timesteps)
 lstm = SimpleLSTM(n_channels=n_channels, seq_len=n_timesteps)
-models = {'MLP':mlp, 'CNN':cnn, 'RNN':rnn, 'LSTM:':lstm}
+
+models = {'MLP':SimpleMLP, 'CNN':SimpleCNN} #, 'RNN':SimpleRNN, 'LSTM':SimpleLSTM}
 
 print('Models defined!')
+
 
 ######################
 # Define CL strategy
 ######################
 
-def load_strategy(model, strategy, **kwargs):
-    """
-    """
-    optimizer = SGD(model.parameters(), lr=0.001, momentum=0.9)
-    criterion = CrossEntropyLoss()
-
-    # Params
-    train_mb_size=100
-    train_epochs=6
-    eval_mb_size=100
-
-    strategies = {'Naive':Naive, 'Joint':JointTraining, 'Cumulative':Cumulative,
-    'EWC':EWC, 'LwF':LwF, 'SI':SynapticIntelligence, 'Replay':Replay, 
-    'GEM':GEM, 'AGEM':AGEM}
-
-    strategy = strategies[strategy]
-
-    model = strategy(
-        model, optimizer, criterion, 
-        train_mb_size=train_mb_size, train_epochs=train_epochs, eval_mb_size=eval_mb_size,
-        **kwargs
-    )
-
-    return model
-
+# Need to rerun multiple times, take averages, shuffle each task data internally
+strategies = {'Naive':{}, 'Cumulative':{}, 'Replay':{'mem_size':10}, 'EWC':{'ewc_lambda':0.01}}
 
 #####################
 # TRAINING
 #####################
 
-# TRAINING LOOP
-def train_method(cl_strategy, scenario):
-    """
-    """
-    print('Starting experiment...')
-    results = []
-    for i, experience in enumerate(scenario.train_stream):
-        print("Start of experience: ", experience.current_experience)
-        #print("Current Classes: ", experience.classes_in_this_experience)
+# Container for metrics for plotting
+res = {m:{s:None for s in strategies.keys()} for m in models.keys()}
 
-        cl_strategy.train(experience)
-        print('Training completed')
-
-        print('Computing accuracy on the whole test set')
-        results.append(cl_strategy.eval(scenario.test_stream))
-
-    return results
-
-# Need to rerun multiple times, take averages, shuffle each task data internally
-kwargs = {'Naive':{}, 'Joint':{}, 'Replay':{'mem_size':10}, 'EWC':{'ewc_lambda':0.01}}
-strategies = kwargs.keys()
-#models = [model] # Need new instance of each model for each training loop or remembers weights
-fig, axes = plt.subplots(len(models), len(strategies))
-
-train_epochs=6
-
-for i, (model_name, model) in enumerate(models.items()):
+for i, (model_name, m) in enumerate(models.items()):
     # Save initial weights for iterating over methods
-    sd = model.state_dict()
-    for j, strategy in enumerate(strategies):
+    #sd = model.state_dict()
+    for j, (strategy_name, args) in enumerate(strategies.items()):
         # Reload initial weights for each method
-        model.load_state_dict(sd)
-        cl_strategy = load_strategy(model, strategy, **kwargs[strategy])
+        #model.load_state_dict(sd)
+        model = models[model_name](n_channels=n_channels, seq_len=n_timesteps)
+        cl_strategy = load_strategy(model, strategy_name, **args)
         results = train_method(cl_strategy, scenario)
-        plot_accuracy(strategy,model_name, results, axes[i,j])
-        clean_plot(i, j, axes[i,j])
+        res[model_name][strategy_name] = results
 
-axes[0,0].get_shared_y_axes().join(*[sp for ax in axes for sp in ax])
-handles, labels = axes[0,0].get_legend_handles_labels()
-axes[0,0].get_legend().remove()
-fig.legend(handles, labels, loc='center right')
 
-fig.supxlabel(' \n\n\n\nEpoch')
-fig.supylabel('Accuracy\n\n\n\n')
+############
+# PLOTTING
+############
 
-plt.show()
+fig, axes = plt.subplots(len(models), len(strategies), sharex=True, sharey=True, figsize=(8,8*(len(models)/len(strategies))))
+
+for i, model in enumerate(models.keys()):
+    for j, strategy in enumerate(strategies.keys()):
+        plot_accuracy(strategy, model, res[model][strategy], axes[i,j])
+        clean_subplot(i, j, axes)
+
+clean_plot(fig, axes)
+
+if False:
+    for _, m in models.items():
+        numel = sum(p.numel() for p in m.parameters() if p.requires_grad)
+        print(numel)
