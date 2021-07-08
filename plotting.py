31598cf8955400a7a85e0087874df1d3b26d9e92
Jacob <47821203+iacobo@users.noreply.github.com>
2021-07-08 18:46:33 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,55 +1,233 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from collections import defaultdict
-
-def plot_accuracy(method, model, results, ax=None):
-    ax = ax or plt.gca()
-
-    acc = defaultdict(list)
-
-    # Get accuracies for each test set per training "experience"
-    for k,v in results.items():
-        if 'Top1_Acc_Exp' in k and '/Exp' in k:
-            new_k = k.split('/')[-1].replace('Exp00','Task ').replace('Exp0','Task ')
-            acc[new_k] = v[1]
-
-    df = pd.DataFrame.from_dict(acc)
-    df.index.rename(f'Epoch', inplace=True)
-    stacked = df.stack().reset_index()
-    stacked.rename(columns={'level_1': 'Task', 0: 'Accuracy'}, inplace=True)
-
-    # Only plot task accuracies after examples have been encountered
-    #stacked = stacked[stacked['Task'].astype(int) <= stacked['Epoch \n (15 epochs per task)'].astype(int)]
-
-    sns.lineplot(data=stacked, x=f'Epoch', y='Accuracy', hue='Task', ax=ax)
-    ax.set_title(method, size=10)
-    ax.set_ylabel(model)
-    ax.set_xlabel('')
-
-def clean_subplot(i, j, axes):
-    ax = axes[i,j]
-    
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-
-    if i>0:
-        ax.set_title('')
-    if i>0 or j>0:
-        ax.get_legend().remove()
-
-    plt.setp(axes, ylim=(0,1))
-
-def clean_plot(fig, axes):
-    handles, labels = axes[0,0].get_legend_handles_labels()
-    axes[0,0].get_legend().remove()
-    fig.legend(handles, labels, loc='center right', title='Task')
-
-    fig.suptitle('Continual Learning model comparison')
-    fig.supxlabel('Epoch')
-    fig.supylabel('Accuracy', x=0)
-    
-    plt.show()
-
 #%%
+
+import torch
+from sklearn.datasets import make_gaussian_quantiles
+import seaborn as sns; sns.set()
+from scipy.io import arff
+import pandas as pd
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+
+# Construct dataset
+
+def generate_experiences(n_timesteps, n_channels, n_tasks, n_samples=2000, test=False):
+
+    if test:
+        n_samples //= 5
+    experiences = [make_gaussian_quantiles(mean=[3*i for _ in range(n_channels*n_timesteps)],
+                                    cov=[3. for _ in range(n_channels*n_timesteps)],
+                                    n_samples=n_samples, n_features=n_channels*n_timesteps,
+                                    n_classes=2, random_state=1)
+                                    for i in range(n_tasks)]
+
+    for i in range(n_tasks):
+        x = torch.FloatTensor(experiences[i][0]).view(-1, n_timesteps, n_channels)
+        y = torch.LongTensor(experiences[i][1])
+        experiences[i] = (x,y)
+
+    return experiences
+
+#############################################################
+# Detecting eye open/closed (0/1) based on phys sensors
+# Split time-series into chunks, target is mode target of chunk.
+#############################################################
+
+def grab_eeg_data():
+    data = arff.loadarff(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\EEG Eye State.arff')
+    df = pd.DataFrame(data[0])
+    df['eyeDetection'] = [int(s.decode()) for s in df['eyeDetection']]
+
+    #14980 obs
+    partitions = len(df)//8
+    trunc = len(df) - len(df)%partitions
+    dfs = np.split(df[:trunc], partitions)
+
+    # Creating tensors
+    target = [df['eyeDetection'].values for df in dfs]
+    target = torch.mode(torch.tensor(target))[0]
+    features = torch.FloatTensor([df.drop('eyeDetection', axis=1).values for df in dfs])
+
+    return features, target
+
+def generate_eeg_experiences(n_tasks):
+    features, target = grab_eeg_data()
+    datalen = features.shape[0]
+
+    if datalen%n_tasks != 0:
+        trunc = datalen - datalen%n_tasks
+        features, target = features[:trunc], target[:trunc]
+
+    train_size = int(0.8*datalen//n_tasks)
+    test_size = datalen//n_tasks - train_size
+    task_sizes = [size for _ in range(n_tasks) for size in (train_size,test_size)]
+    
+    # Use sklearn train_test_split
+    features = features.split(task_sizes)
+    target = target.split(task_sizes)
+
+    features = list(features)
+
+    for i in range(0,n_tasks*2,2):
+        features[i] = features[i] + torch.ones_like(features[i])*i*100
+        features[i+1] = features[i+1] + torch.ones_like(features[i+1])*i*100
+
+    return list(zip(features,target))[::2], list(zip(features,target))[1::2]
+
+def generate_permuted_eeg_experiences(n_tasks):
+    features, target = grab_eeg_data()
+
+    perm_feats = [features[:,:,torch.randperm(features.shape[2])] for i in range(n_tasks)]
+
+    datalen = len(target)
+    train_size = int(0.8*datalen)
+    
+    return [(f[:train_size],target[:train_size]) for f in perm_feats], [(f[train_size:],target[train_size:]) for f in perm_feats]
+
+# Check exp[1][0] == exp[0][0] shows weird all True col
+
+#####################################################################
+
+def load_activity_data(test=False):
+    if not test:
+        df = pd.read_csv(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\train.csv')
+    else:
+        df = pd.read_csv(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\test.csv')
+    
+    df['Target'] = df['Activity'].str.contains('WALKING')
+    tasks = [None,None,None]
+    tasks[0] = df[df['Activity'].isin(['LAYING','WALKING'])]
+    tasks[2] = df[df['Activity'].isin(['SITTING','WALKING_DOWNSTAIRS'])]
+    tasks[1] = df[df['Activity'].isin(['STANDING','WALKING_UPSTAIRS'])]
+
+    for i in range(len(tasks)):
+        #plt.plot(list(range(len(tasks[i]))), tasks[i]['Activity'])
+        #plt.show()
+
+        tasks[i] = tasks[i].drop(['subject', 'Activity'], axis=1)
+        tasks[i] = tasks[i][['angle(X,gravityMean)', 'angle(Y,gravityMean)', 'Target']]
+
+        partitions = len(tasks[i])//6
+        trunc = len(tasks[i]) - len(tasks[i])%partitions
+        dfs = np.split(tasks[i][:trunc], partitions)
+
+        target = [d['Target'].values for d in dfs]
+        target = torch.mode(torch.LongTensor(target))[0]
+        features = torch.FloatTensor([d.drop('Target', axis=1).values for d in dfs])
+
+        tasks[i] = (features, target)
+    return tasks
+
+
+
+########################
+# eICU DATASET
+########################
+
+def load_eicu(drop_dupes=False):
+    # Load tables
+    df_p = pd.read_csv(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\eICU\patient.csv')
+    df_h = pd.read_csv(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\eICU\hospital.csv')
+    # Cleaning
+    df_p['age'] = pd.to_numeric(df_p['age'].replace('> 89',90))
+    df_p['ethnicity_coarse'] = np.where(df_p['ethnicity']=='Caucasian', 'Caucasian', 'BAME')
+    df_p['region'] = pd.merge(df_p,df_h,how='left',on='hospitalid')['region']
+
+    # Grabbing relevant cols
+    # 'patientunitstayid', 'patienthealthsystemstayid', 
+    df_p = df_p[['uniquepid', 'patientunitstayid', 'patienthealthsystemstayid', 'hospitaldischargeoffset', 'hospitaldischargestatus', 'gender', 'age', 'ethnicity', 'ethnicity_coarse', 'region']]
+
+    # Some patients have multiple admissions in different hospital regions, hence mismatch
+    if drop_dupes:
+        df_p = df_p[['uniquepid', 'hospitaldischargestatus', 'gender', 'age', 'ethnicity', 'ethnicity_coarse', 'region']]
+        len_distinct = len(df_p.drop_duplicates())
+        len_ids = len(df_p.groupby('uniquepid'))
+        print(f'There are {len_distinct} unique rows, and {len_ids} unique patient ids.')
+        df_p = df_p.drop_duplicates()
+
+    return df_p
+
+def plot_demos():
+
+    df = load_eicu(drop_dupes=True)
+
+    fig, axes = plt.subplots(3,2, sharey=True, figsize=(18,18), squeeze=False)
+
+    df['gender'].value_counts().plot.bar(ax=axes[0,0], rot=0, title='Gender')
+    df['ethnicity'].value_counts().plot.bar(ax=axes[1,0], rot=0, title='Ethnicity')
+    df['ethnicity_coarse'].value_counts().plot.bar(ax=axes[1,1], rot=0, title='Ethnicity (coarse)')
+    df['age'].plot.hist(bins=20, label='age', ax=axes[0,1], title='Age')
+    df['region'].value_counts().plot.bar(ax=axes[2,0], rot=0, title='Region (North America)')
+    df['hospitaldischargestatus'].value_counts().plot.bar(ax=axes[2,1], rot=0, title='Outcome')
+    plt.show(); plt.close()
+
+def load_eicu_timeseries():
+
+    df_p = load_eicu()
+    df_v = pd.read_csv(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\eICU\vitalPeriodic.csv')
+
+    df = pd.merge(df_p, df_v, on='patientunitstayid')
+
+    #28 day mortality
+    df['Target'] = (df['hospitaldischargeoffset'] - df['observationoffset'] < 28*24*60) & (df['hospitaldischargestatus'] == 'Expired')
+
+    df['Target'].value_counts().plot.bar(rot=0, title='Mortality within 28 days of observation')
+    plt.show(); plt.close()
+
+
+    return df
+
+def eicu_to_tensor(demographic):
+    df = load_eicu_timeseries()
+
+    tasks = []
+
+    if demographic == 'gender':
+        tasks.append(df[df['gender']=='Male'])
+        tasks.append(df[df['gender']=='Female'])
+    elif demographic == 'age':
+        tasks.append(df[df['age'].between(0,30)])
+        tasks.append(df[df['age'].between(30,60)])
+        tasks.append(df[df['age'].between(60,100)])
+    elif demographic == 'ethnicity':
+        tasks.append(df[df['ethnicity']=='Caucasian'])
+        tasks.append(df[df['ethnicity']=='African American'])
+        tasks.append(df[df['ethnicity']=='Hispanic'])
+        tasks.append(df[df['ethnicity']=='Asian'])
+        tasks.append(df[df['ethnicity']=='Native American'])
+        tasks.append(df[df['ethnicity']=='Other/Unknown'])
+    elif demographic == 'ethnicity_coarse':
+        tasks.append(df[df['ethnicity_coarse']=='Caucasian'])
+        tasks.append(df[df['ethnicity']=='BAME'])
+
+
+    for i in range(len(tasks)):
+        seq_len = 6
+
+        tasks[i] = tasks[i][['temperature', 'sao2', 'heartrate', 'respiration',
+       'cvp', 'etco2', 'systemicsystolic', 'systemicdiastolic', 'systemicmean',
+       'pasystolic', 'padiastolic', 'pamean', 'st1', 'st2', 'st3', 'icp',
+       'Target']]
+
+        partitions = len(tasks[i])//seq_len
+        trunc = len(tasks[i]) - len(tasks[i])%partitions
+        dfs = np.split(tasks[i][:trunc], partitions)
+
+        target = [d['Target'].values for d in dfs]
+        target = torch.LongTensor(target).max(axis=1)[0]
+        features = torch.FloatTensor([d.drop('Target', axis=1).values for d in dfs])
+
+        tasks[i] = (features, target)
+
+    return tasks
+
+# %%
+
+#plot_demos()
+#df = load_eicu_timeseries()
+#tasks = eicu_to_tensor(df, demographic='ethnicity')
+
+# %%
+
+# %%
