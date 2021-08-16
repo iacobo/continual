4e13da1def4098258cd01bd869394d008085d591
Jacob <47821203+iacobo@users.noreply.github.com>
2021-08-16 11:26:59 +0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,7 @@
 from ray import tune
 import numpy as np
 
+# Conditional hyper-param functions
 def get_dropout_from_n_layers(spec):
     """
     Has dropout of 0 if n_layers==1, else random dropout.
@@ -12,14 +13,14 @@ def get_dropout_from_n_layers(spec):
 
 def get_decay_from_ewc_mode(spec):
     """
-    Only return decay factor if usig Online EWC.
+    Return decay factor if usig Online EWC.
     """
     if spec.config.strategy.mode=='online':
-        return np.random.uniform(0,1)
+        return 0.1 * np.random.randint(0,11)
     else:
         return None
 
-# Generic hyperparameter search-space
+# Hyperparameter search-space
 config_generic = {'lr':tune.choice([1e-4,1e-3,1e-2,1e-1]), 
                   'optimizer':tune.choice(['SGD','Adam']),
                   'hidden_dim':tune.choice([64,128,256,512,1024]),
@@ -27,19 +28,30 @@ config_generic = {'lr':tune.choice([1e-4,1e-3,1e-2,1e-1]),
                   'train_mb_size':tune.choice([32,64,128,256,512,1024])
                   }
 
-config_model = {# JA: use ModuleList(?) to parameterise n_layers for MLP and CNN
-                'CNN':{'nonlinearity':tune.choice(['tanh', 'relu'])},
-                'MLP':{'dropout':tune.choice([0,0.1,0.2,0.3,0.4,0.5]), 'nonlinearity':tune.choice(['tanh', 'relu'])},
-                'RNN':{'n_layers':tune.choice([1,2,3,4]), 'dropout':tune.sample_from(get_dropout_from_n_layers), 'bidirectional':tune.choice([True,False]), 'nonlinearity':tune.choice(['tanh', 'relu'])},
-                'LSTM':{'n_layers':tune.choice([1,2,3,4]), 'dropout':tune.sample_from(get_dropout_from_n_layers), 'bidirectional':tune.choice([True,False])}
+# JA: use ModuleList(?) to parameterise n_layers for MLP and CNN
+config_model = {'CNN':{'nonlinearity':tune.choice(['tanh', 'relu'])},
+                'MLP':{'dropout':tune.choice([0,0.1,0.2,0.3,0.4,0.5]), 
+                       'nonlinearity':tune.choice(['tanh', 'relu'])},
+                'RNN':{'n_layers':tune.choice([1,2,3,4]), 
+                       'dropout':tune.sample_from(get_dropout_from_n_layers), 
+                       'bidirectional':tune.choice([True,False]), 
+                       'nonlinearity':tune.choice(['tanh', 'relu'])},
+                'LSTM':{'n_layers':tune.choice([1,2,3,4]), 
+                        'dropout':tune.sample_from(get_dropout_from_n_layers), 
+                        'bidirectional':tune.choice([True,False])}
                 }
 
 config_cl ={'Replay':{'mem_size':tune.choice([4,16,32])},
             'GDumb':{'mem_size':tune.choice([4,16,32])},
-            'EWC':{'ewc_lambda':tune.choice([1e-3,1e-2,1e-1,1e0,1e1,1e2]), 'mode':tune.choice(['separate','online']), 'decay_factor':tune.sample_from(get_decay_from_ewc_mode)},
+            'EWC':{'ewc_lambda':tune.choice([1e-3,1e-2,1e-1,1e0,1e1,1e2]), 
+                   'mode':tune.choice(['separate','online']), 
+                   'decay_factor':tune.sample_from(get_decay_from_ewc_mode)},
             'SI':{'si_lambda':tune.choice([1e-3,1e-2,1e-1,1e0,1e1,1e2])},
-            'LwF':{'alpha':tune.choice([1e-3,1e-2,1e-1,1e0,1e1,1e2]), 'temperature':tune.quniform(0,3,0.5)},
-            'GEM':{'patterns_per_exp':tune.choice([4,16,32]), 'memory_strength':tune.quniform(0,1,0.1)},
-            'AGEM':{'patterns_per_exp':tune.choice([4,16,32]), 'sample_size':tune.choice([4,16,32])}
+            'LwF':{'alpha':tune.choice([1e-3,1e-2,1e-1,1e0,1e1,1e2]), 
+                   'temperature':tune.quniform(0,3,0.5)},
+            'GEM':{'patterns_per_exp':tune.choice([4,16,32]), 
+                   'memory_strength':tune.quniform(0,1,0.1)},
+            'AGEM':{'patterns_per_exp':tune.choice([4,16,32]), 
+                    'sample_size':tune.choice([4,16,32])}
             #'CoPE':
             }
