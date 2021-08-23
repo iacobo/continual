97ffc0ccf01f366e92362b7543f5cf2ad6675540
Jacob <47821203+iacobo@users.noreply.github.com>
2021-08-23 16:40:49 +0300

    Lint updates.

--- main.py
+++ main.py
@@ -8,67 +8,68 @@ CONFIG_DIR = Path(__file__).parents[0] / 'config'
 
 def main(args):
     if args.models=='all':
-        args.models = ['MLP', 'CNN', 'RNN', 'LSTM']
+        args.models = ['MLP','CNN','RNN','LSTM']
 
+    # JA: INVESTIGATE MAS, 'AGEM' (num samples > mem?)!!!
     if args.strategies=='all':
-        args.strategies = ['Naive', 'Cumulative', 'EWC', 'SI', 'LwF', 'Replay', 'GDumb', 'GEM'] # JA: INVESTIGATE MAS, 'AGEM' (num samples > mem?)!!!
+        args.strategies = ['Naive','Cumulative','EWC','SI','LwF','Replay','GDumb','GEM']
 
     # Hyperparam optimisation over validation data for first 2 tasks
     if args.validate:
-        best_params = training.main(data=args.data, 
-                                    demo=args.experiment, 
-                                    models=args.models, 
-                                    strategies=args.strategies, 
-                                    config_generic=config.config_generic, 
-                                    config_model=config.config_model, 
-                                    config_cl=config.config_cl, 
+        best_params = training.main(data=args.data,
+                                    demo=args.experiment,
+                                    models=args.models,
+                                    strategies=args.strategies,
+                                    config_generic=config.config_generic,
+                                    config_model=config.config_model,
+                                    config_cl=config.config_cl,
                                     validate=True)
 
     # Load previously tuned hyper-param config
     else:
         with open(CONFIG_DIR / f'config_{args.data}_{args.experiment}.json') as json_file:
             best_params = json.load(json_file)
-    
+
     # Train and test over all tasks (using optimised hyperparams)
-    training.main(data=args.data, 
-                  demo=args.experiment, 
-                  models=args.models, 
+    training.main(data=args.data,
+                  demo=args.experiment,
+                  models=args.models,
                   strategies=args.strategies,
                   config_cl=best_params)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', 
-                        type=str, 
-                        default='fiddle_mimic3', 
-                        choices=['fiddle_mimic3','fiddle_eicu','random'], 
+    parser.add_argument('--data',
+                        type=str,
+                        default='fiddle_mimic3',
+                        choices=['fiddle_mimic3','fiddle_eicu','random'],
                         help='Dataset to use.')
-    parser.add_argument('--outcome', 
-                        type=str, 
-                        default='mortality_48h', 
-                        choices=['ARF_4h','ARF_12h','shock_4h','shock_12h','mortality_48h'], 
+    parser.add_argument('--outcome',
+                        type=str,
+                        default='mortality_48h',
+                        choices=['ARF_4h','ARF_12h','shock_4h','shock_12h','mortality_48h'],
                         help='Outcome to predict.')
-    parser.add_argument('--experiment', 
-                        type=str, 
-                        default='age', 
-                        choices=['time_month','time_season','time_year','region','hospital','age','sex','ethnicity'], 
+    parser.add_argument('--experiment',
+                        type=str,
+                        default='age',
+                        choices=['time_season','region','hospital','age','sex','ethnicity'],
                         help='Experiment to run.') # Domain incremental
-    parser.add_argument('--strategies', 
-                        type=str, 
-                        default='all', 
-                        choices=['Naive', 'Cumulative', 'EWC', 'SI', 'LwF', 'Replay', 'GDumb', 'GEM', 'AGEM'], 
+    parser.add_argument('--strategies',
+                        type=str,
+                        default='all',
+                        choices=['Naive','Cumulative','EWC','SI','LwF','Replay','GDumb','GEM','AGEM'],
                         nargs='+',
                         help='Continual learning strategy(s) to evaluate.')
-    parser.add_argument('--models', 
-                        type=str, 
-                        default='all', 
-                        choices=['MLP','CNN','RNN','LSTM'], 
+    parser.add_argument('--models',
+                        type=str,
+                        default='all',
+                        choices=['MLP','CNN','RNN','LSTM'],
                         nargs='+',
                         help='Model(s) to evaluate.')
-    parser.add_argument('--validate', 
-                        action='store_const', 
-                        const=True, 
-                        default=False, 
+    parser.add_argument('--validate',
+                        action='store_const',
+                        const=True,
+                        default=False,
                         help='Tune hyperparameters.')
     args = parser.parse_args()
 
--- tests.py
+++ tests.py
@@ -1,8 +1,7 @@
 import math
-import torch
 import unittest
 import itertools
-from pathlib import Path
+import torch
 
 from utils import models, data_processing
 
@@ -12,11 +11,15 @@ N_VARS = (2,10,30,100)
 N_CLASSES = (2,3,4,10)
 
 def magnitude(value):
-    if (value == 0): return 0
-    return int(math.floor(math.log10(abs(value))))
+    if value == 0: 
+        return 0
+    else:
+        return int(math.floor(math.log10(abs(value))))
 
-# Model definition tests
 class TestModelMethods(unittest.TestCase):
+    """
+    Model definition tests.
+    """
 
     def test_modeloutputshape(self):
         """
@@ -42,36 +45,19 @@ class TestModelMethods(unittest.TestCase):
             for n_vars in N_VARS:
                 for n_classes in N_CLASSES:
                     simple_models = models.MODELS.values()
-                    n_params = [sum(p.numel() for p in m(seq_len=seq_len, n_channels=n_vars, output_size=n_classes).parameters() if p.requires_grad) for m in simple_models]
+                    n_params = [sum(p.numel() 
+                                for p in m(seq_len=seq_len, n_channels=n_vars, output_size=n_classes).parameters() 
+                                if p.requires_grad) 
+                                for m in simple_models]
                     param_magnitudes = [magnitude(p) for p in n_params]
                     # RNN/LSTM order bigger
                     self.assertTrue(max(param_magnitudes)-min(param_magnitudes)<=1)
 
 
-
-# Data loading tests: TO SHELVE AFTER FIDDLE INCORPORATED
 class TestDataLoadingMethods(unittest.TestCase):
-
-    def ttest_ids(self):
-        """
-        Checking patient ID's are unique.
-        """
-        data_dir = Path(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\eICU')
-        df = data_processing.load_eicu(root=data_dir)
-        self.assertEqual(len(df), len(df.groupby('patientunitstayid')))
-        
-    def ttest_tasktargets(self):
-        """
-        Check all task splits have examples of both classes.
-        """
-        data_dir = Path(r'C:\Users\jacob\OneDrive\Documents\code\cl code\ehr\data\eICU')
-        demographics = ['age', 'gender', 'ethnicity', 'region', 'hospitaldischargeyear']
-        for demo in demographics:
-            tasks = data_processing.eicu_to_tensor(demo, root=data_dir)
-            for i, task in enumerate(tasks):
-                labels = torch.LongTensor([0,1])
-                data_labels = task[1].unique()
-                self.assertTrue(data_labels.equal(labels), f"Demographic {demo}, task {i} has unexpected labels: {data_labels}")
+    """
+    Data loading tests.
+    """
 
     def test_modalfeatvalfromseq(self):
         """
@@ -81,8 +67,8 @@ class TestDataLoadingMethods(unittest.TestCase):
             for seq_len in SEQ_LENS:
                 for n_feats in N_VARS:
                     for i in range(n_feats):
-                        x = torch.randint(0,1,(n,seq_len,n_feats)).clone().detach().numpy()
-                        modes = data_processing.get_modes(x,feat=i)
+                        sim_data = torch.randint(0,1,(n,seq_len,n_feats)).clone().detach().numpy()
+                        modes = data_processing.get_modes(sim_data,feat=i)
                         self.assertEqual(modes.shape, torch.Size([n]))
 
 # CL task split tests
